2f9b731b5750eded5f7523a3519089d0c98b71e2
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2025-01-11 01:32:33 +0800

    feat: 注视增加

--- src/main/java/com/example/demo/service/impl/DyServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DyServiceImpl.java
@@ -7,12 +7,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
