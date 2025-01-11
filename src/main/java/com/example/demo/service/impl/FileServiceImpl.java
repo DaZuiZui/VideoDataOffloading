c94b853286777c9c16349c22f86157659bb7c893
hln <128910620+1920604577@users.noreply.github.com>
2025-01-11 21:44:22 +0800

    文件下载 && 根据userId查询文件列表
    文件下载 && 根据userId查询文件列表

--- src/main/java/com/example/demo/service/impl/FileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/FileServiceImpl.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
 
 /**
  * 文件系统
@@ -67,11 +67,60 @@ public class FileServiceImpl implements FileService {
 
     @Override
     public R downFile(String filename) {
-        return null;
+        String fullPath = Paths.get(filePath + "/"+userId, filename).toString();
+        File file = new File(fullPath);
+        if (!file.exists()) {
+            return R.fail("文件不存在");
+        }
+
+        try (InputStream inStream = new FileInputStream(file);
+             OutputStream outStream = new FileOutputStream(filePath + "/" + UUID.randomUUID())) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inStream.read(buffer)) != -1) {
+                outStream.write(buffer, 0, bytesRead);
+            }
+
+            return R.ok("下载成功");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return R.fail("下载失败");
+        }
     }
 
     @Override
     public R getFileListByUserId() {
-        return null;
+
+        String fullPath = Paths.get(filePath+"/"+userId).toString();
+        File userFolder = new File(fullPath);
+
+        if (!userFolder.exists() || !userFolder.isDirectory()) {
+            return R.fail("用户文件夹不存在");
+        }
+
+        // 文件列表
+        List<Map<String, Object>> fileList = new ArrayList<>();
+
+        // 获取该用户文件夹下所有文件
+        File[] files = userFolder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    // 每个文件的详细信息
+                    Map<String, Object> fileDetails = new HashMap();
+                    fileDetails.put("file", file); // 文件对象
+                    fileDetails.put("name", file.getName()); // 文件名
+                    fileDetails.put("remark", "这是一个文件备注"); // 备注
+
+                    // 将文件信息添加到列表中
+                    fileList.add(fileDetails);
+                }
+            }
+        }
+
+        return R.ok(fileList);
     }
 }
